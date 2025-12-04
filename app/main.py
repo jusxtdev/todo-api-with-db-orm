46aa0c690579ab15bac62ac910f1484364e7debb
Dev-Prajapati25 <pdev25214@gmail.com>
2025-12-04 11:34:21 +0530

    Added POST User route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.database import init_db
-from app.routers import task
+from app.routers import task, user
 
 app = FastAPI()
 
@@ -10,4 +10,5 @@ def on_startup():
     init_db()
 
 app.include_router(task.router)
+app.include_router(user.router)
     
